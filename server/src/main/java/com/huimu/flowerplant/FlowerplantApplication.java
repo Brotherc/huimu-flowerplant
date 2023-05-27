61d25f20d360991be223b2b10d92a4bd55c44a75
liuchunsheng <chunsheng.liu@eingsoft.com>
2023-05-27 19:38:28 +0800

    初始化项目

---
+++ server/src/main/java/com/huimu/flowerplant/FlowerplantApplication.java
@@ -0,0 +1,13 @@
+package com.huimu.flowerplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerplantApplication.class, args);
+	}
+
+}
