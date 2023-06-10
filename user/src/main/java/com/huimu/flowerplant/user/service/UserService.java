0380e2a7c4c015e68816c7a90e662c0933bcf690
liuchunsheng <chunsheng.liu@eingsoft.com>
2023-06-10 21:05:48 +0800

    集成:mybatis-plugs

--- user/src/main/java/com/huimu/flowerplant/user/service/UserService.java
+++ user/src/main/java/com/huimu/flowerplant/user/service/UserService.java
@@ -1,12 +1,13 @@
 package com.huimu.flowerplant.user.service;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.huimu.flowerplant.user.dao.UserMapper;
 import com.huimu.flowerplant.user.model.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService extends ServiceImpl<UserMapper, User> {
 
     @Autowired
     private UserMapper userMapper;
